6b6a8eaabb6b545e35843c0129ad875b12769f42
Slawek-J <slawomir.jarzynka@gmail.com>
2022-11-02 22:30:33 +0000

    rende rpins

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,5 +1,6 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'
+import {SimpleAddEntity} from 'types'
 import '../../utils/fix-map.icon'
 
 import 'leaflet/dist/leaflet.css'
@@ -8,19 +9,38 @@ import { SearchContext } from '../../context/search.context'
 
 export const Map = () => {
     const {search} = useContext(SearchContext)
+    const [ads, setAds] = useState<SimpleAddEntity[]>([])
 
     useEffect(() => {
-        console.log(`Make a request: ${search}`);
+        (async() => {
+            const data = await(await fetch("http://localhost:3001/ad/search")).json()
+            setAds(data);
+            
+        })()
+    }, [])
+
+    // useEffect(() => {
+    //     console.log(`Make a request: ${search}`);
         
-    }, [search])
+    // }, [search])
   return (
     <div className="map">
-        <h1>Search for: {search}</h1>
         <MapContainer center={[53.34110, -6.25251]} zoom={14}>
             <TileLayer 
                 url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
                 attribution="&copy; <a href='https://openstreetmap.org/copyright'>OpenStreeTMap</a> & contributors"
             />
+            {
+                ads.map(ad => {
+                    return (
+                        <Marker key={ad.id} position={[ad.lat, ad.lon]}>
+                            <Popup>
+                                <h2>{ad.id}</h2>
+                            </Popup>
+                        </Marker>
+                    )
+                })
+            }
             <Marker position={[53.34110, -6.25251]}>
                 <Popup>
                     <h2>National Gallery of Ireland</h2>
